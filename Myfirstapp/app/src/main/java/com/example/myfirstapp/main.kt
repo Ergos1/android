8207a35d71acb9069f2e2346ba1bfd9e32c461a3
ergossteam <ergossteam@gmail.com>
2022-02-24 03:45:32 +0600

    first hw

---
+++ Myfirstapp/app/src/main/java/com/example/myfirstapp/main.kt
@@ -0,0 +1,13 @@
+package com.example.myfirstapp
+
+import android.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
